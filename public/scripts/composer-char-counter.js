925192f57608c7b4025ad2179264bb2e73a217e9
Amaal Ali <amaalalikhadija@gmail.com>
2023-01-31 17:04:17 -0500

    created webpage for tweeter add functionality and css styling

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  
+  console.log("We are ready")
+  $("#tweet-text").on("input",function () {
+    let maxCount = 140;
+    // console.log($(this).val().length);
+    let count = $('.tweet-footer').find('.counter');
+    console.log(count);
+    let countChar = $(this).val().length;
+    let countRemainder = maxCount - countChar;
+    count.text(countRemainder)
+    if (countChar > maxCount) {
+      count.addClass("countLimit");
+    } else {
+      count.removeClass("countLimit");
+    }
+    // console.log("You have ikeypress ",$(this).val())
+  });
+});
+
+  
+  
+  
+  
+  // document.addEventListener("keypress", (event) => {
+  //         // console.log(event);
+  //         console.log("You have ikeypress ",this.event);
+  //       });
+
+   
